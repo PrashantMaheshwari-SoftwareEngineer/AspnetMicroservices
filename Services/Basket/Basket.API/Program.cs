686b342431d8defaa0980cb54430360b672a8c30
PrashantMaheshwari-SoftwareEngineer <prashantmaheshwari@outlook.com>
2021-11-29 08:26:47 +0530

    Basket API

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,17 +1,19 @@
-using Catalog.API.Data;
-using Catalog.API.Repositories;
+using Basket.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Host.ConfigureAppConfiguration(app => app.AddJsonFile(""));
-
 // Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddStackExchangeRedisCache(
+    options =>
+    {
+        options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+    });
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
